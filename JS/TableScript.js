279c0749a03d0e28b61546972076816cc2a0dd95
Stefalgo <197377429+stefalgo@users.noreply.github.com>
2025-05-23 01:51:41 +0300

    Update TableScript.js

--- JS/TableScript.js
+++ JS/TableScript.js
@@ -1,32 +1,20 @@
 const classes = [
-	"alkali",
-	"alkaline",
-	"nonmetal",
-	"transition",
-	"unknown",
-	"lanthanoid",
-	"actinoid",
-	"metalloid",
-	"poor",
-	"noble"
+	{en: "alkali", gr: "Αλκαλικά μέταλλα"},
+	{en: "alkaline", gr: "Αλκαλικές γαίες"},
+	{en: "nonmetal", gr: "Αμέταλλο"},
+	{en: "transition", gr: "Μετάλλα μετάπτωσης"},
+	{en: "unknown", gr: "Άγνωστο"},
+	{en: "lanthanoid", gr: "Λανθανίδα"},
+	{en: "actinoid", gr: "Ακτινίδα"},
+	{en: "metalloid", gr: "Μεταλλοειδές"},
+	{en: "poor", gr: "Φτωχό μέταλλο"},
+	{en: "noble", gr: "Ευγενές αέριο"}
 ];
-const classesGreek = [
-	"Αλκαλική",
-	"Αλκαλική γαία",
-	"Αμέταλλο",
-	"Μετάλλα μετάπτωσης",
-	"Άγνωστο",
-	"Λανθανίδα",
-	"Ακτινίδα",
-	"Μεταλλοειδές",
-	"Φτωχό μέταλλο",
-	"Ευγενές αέριο"
-];
-
-const closeUp = document.getElementById('CloseUp');
-var elementAtomicNumber;
 
 const elementData = {};
+const closeUp = document.getElementById('CloseUp');
+
+let elementAtomicNumber;
 
 fetch("JsonData/Elements.json")
 //fetch("https://raw.githubusercontent.com/stefalgo/My-Periodic-Table/main/JsonData/Elements.json")
@@ -35,7 +23,7 @@ fetch("JsonData/Elements.json")
     Object.assign(elementData, jsonData);
 	console.log("JSON loaded successfully:");
 
-	var urlSelectedElement = URL_readParam("SelectedElement")
+	let urlSelectedElement = URL_readParam("SelectedElement")
 
 	if (urlSelectedElement) {
 		elementClicked(document.querySelector(`[data-atomic="${urlSelectedElement}"]`));
@@ -46,19 +34,18 @@ fetch("JsonData/Elements.json")
   .catch(error => console.error("Error loading JSON:", error));
 
 function energyLevel(elementName) {
-	closeUp.querySelector('small').innerHTML = '';
+	const smallElement = closeUp.querySelector('small');
+	smallElement.innerHTML = '';
 
 	if (elementData.hasOwnProperty(elementName)) {
 
-		var element = elementData[elementName];
+		let element = elementData[elementName];
 
-		for (var i = 0; i < element.energyLevels.length; i++) {
-
-			var spanElement = document.createElement('span');
-			spanElement.textContent = (element.energyLevels[i]);
-			closeUp.querySelector('small').appendChild(spanElement);
-			//console.log('Energy Level ' + (i + 1) + ': ' + element.energyLevels[i]);
-		}
+	for (const level of element.energyLevels) {
+		let spanElement = document.createElement('span');
+		spanElement.textContent = level;
+		smallElement.appendChild(spanElement);
+	}
 	} else {
 		console.log('Element with name "' + elementName + '" does not exist.');
 	}
@@ -103,14 +90,12 @@ function generateAtom(atomicNumber) {
 
 function showElementData(target) {
 	const targetClasses = Array.from(target.classList);
-
-	//function update(onElement) {
 	const atomic = closeUp.querySelector('.closeUp-atomic');
 	const symbol = closeUp.querySelector('.closeUp-shortName');
 	const name = closeUp.querySelector('.closeUp-name');
 	const mass = closeUp.querySelector('.closeUp-mass');
 	const allChildElements = target.children;//getChildrenElements(onElement);
-	const matchingClass = targetClasses.find(cls => classes.includes(cls));
+	const matchingClassObj = classes.find(c => targetClasses.includes(c.en));
 				
 	atomic.textContent = elementAtomicNumber;
 	energyLevel(elementAtomicNumber);
@@ -120,107 +105,101 @@ function showElementData(target) {
 	symbol.textContent = elementData[elementAtomicNumber].shortName;
 	mass.textContent = allChildElements[3].textContent;
 	closeUp.classList = ["elementStyle"];
-	if (matchingClass) {
-		closeUp.classList.add(matchingClass);
+	if (matchingClassObj) {
+		closeUp.classList.add(matchingClassObj.en);
 	}
-	//}
-	/*
-	if (targetClasses.some(cls => classes.includes(cls))) {
-		update(target);
-	}
-	*/
 }
 
 //-----------------------------------------------------------------------------------------------
 
 function infoElement() {
 	const infoPopup = document.getElementById('infoPopup');
-	var element;
+	const name = infoPopup.querySelector('.popup-name');
+	const atomic = infoPopup.querySelector('.popup-atomic');
+	const energyLevels = infoPopup.querySelector('.popup-energyLevels');
+	const discovered = infoPopup.querySelector('.popup-discovered');
+	const block = infoPopup.querySelector('.popup-block');
+	const elementClass = infoPopup.querySelector('.popup-class');
+	const wikipediaLink = infoPopup.querySelector('.popup-wikipediaLink');
+	const downloadPDF = infoPopup.querySelector('.popup-pdfDownload');
+	const closeUp2 = copyCloseUp();
+
+	let element;
+	let link;
+	let pdf;
+
+	function copyCloseUp() {
+		let clonedElement = closeUp.cloneNode(true);
+		let closeUp2 = document.getElementById('CloseUp2');
+		closeUp2.innerHTML = '';
+		closeUp2.appendChild(clonedElement);
+		return clonedElement;
+	}
+
+	function closePopup(event) {
+		infoPopup.style.display = "none";
+		wikipediaLink.href = "";
+		downloadPDF.href = "";
+		closeUp2.removeEventListener('click', wikipediaIframeOpen);
+		infoPopup.querySelector('.close').removeEventListener('click', closePopup);
+	}
+
+	function wikipediaIframeOpen(event) {
+		openLinkInIframe(element);
+	}
 				
 	if (typeof elementAtomicNumber !== 'undefined') {
 		element = elementAtomicNumber;
 	} else {
 		element = '1';
 	}
-	//function updateInfoPopup() {
-		function copyCloseUp() {
-			var clonedElement = closeUp.cloneNode(true);
-			var closeUp2 = document.getElementById('CloseUp2');
-			closeUp2.innerHTML = '';
-			closeUp2.appendChild(clonedElement);
-			return clonedElement
-		}
-		const closeUp2 = copyCloseUp()
-					
-		const name = infoPopup.querySelector('.popup-name');
-		const atomic = infoPopup.querySelector('.popup-atomic');
-		const energyLevels = infoPopup.querySelector('.popup-energyLevels');
-		const discovered = infoPopup.querySelector('.popup-discovered');
-		const block = infoPopup.querySelector('.popup-block');
-		const elementClass = infoPopup.querySelector('.popup-class');
-		const wikipediaLink = infoPopup.querySelector('.popup-wikipediaLink');
-		const downloadPDF = infoPopup.querySelector('.popup-pdfDownload');
-		
-		var link;
-		var pdf;
-		
-		if (elementData[element].linkElementName) {
-			link = "https://el.wikipedia.org/wiki/" + elementData[element].linkElementName;
-			pdf = "https://el.wikipedia.org/api/rest_v1/page/pdf/" + elementData[element].linkElementName;
-		} else {
-			link = "https://el.wikipedia.org/wiki/" + elementData[element].name;
-			pdf = "https://el.wikipedia.org/api/rest_v1/page/pdf/" + elementData[element].name;
-		}
-		
-		name.innerHTML = elementData[element].name;
-		atomic.innerHTML = element;
-					
-		energyLevels.innerHTML = elementData[element].energyLevels.join(', ');
-		discovered.innerHTML = elementData[element].discovered;
+	
+	if (elementData[element].linkElementName) {
+		link = "https://el.wikipedia.org/wiki/" + elementData[element].linkElementName;
+		pdf = "https://el.wikipedia.org/api/rest_v1/page/pdf/" + elementData[element].linkElementName;
+	} else {
+		link = "https://el.wikipedia.org/wiki/" + elementData[element].name;
+		pdf = "https://el.wikipedia.org/api/rest_v1/page/pdf/" + elementData[element].name;
+	}
+	
+	name.innerHTML = elementData[element].name;
+	atomic.innerHTML = element;
+				
+	energyLevels.innerHTML = elementData[element].energyLevels.join(', ');
+	discovered.innerHTML = elementData[element].discovered;
 
-		block.innerHTML = elementData[element].block + '-τομέας';
-		elementClass.innerHTML = classesGreek[classes.indexOf(elementData[element].class)] ?? "Άγνωστη κατηγορία";//closeUp2.classList[1];
-		
-		function wikipediaIframeOpen(event) {
-			openLinkInIframe(element);
-		}
-					
-		function closePopup(event) {
-			infoPopup.style.display = "none";
-			wikipediaLink.href = "";
-			downloadPDF.href = "";
-			closeUp2.removeEventListener('click', wikipediaIframeOpen);
-			infoPopup.querySelector('.close').removeEventListener('click', closePopup);
-		}
-					
-		closeUp2.addEventListener('click', wikipediaIframeOpen);
-		infoPopup.querySelector('.close').addEventListener('click', closePopup);
-		wikipediaLink.href = link;
-		downloadPDF.href = pdf;
-	//}
+	block.innerHTML = elementData[element].block;// + '-τομέας';
+	elementClass.innerHTML = classes.find(c => c.en === elementData[element].class)?.gr ?? "Άγνωστη κατηγορία";
+				
+	closeUp2.addEventListener('click', wikipediaIframeOpen);
+	infoPopup.querySelector('.close').addEventListener('click', closePopup);
+	wikipediaLink.href = link;
+	downloadPDF.href = pdf;
 
 	infoPopup.style.display = "block";
-	//updateInfoPopup();
 }
 
 function elementClicked(clickedElement) {
-	if (clickedElement.hasAttribute('data-linkedElement')) {
-		elementAtomicNumber = clickedElement.getAttribute('data-linkedElement');
-		showElementData(document.querySelector(`[data-atomic="${elementAtomicNumber}"]`));
-		return;
-	} else {
-		elementAtomicNumber = clickedElement.getAttribute("data-atomic");
-		showElementData(clickedElement);
-		return;
-	}
+	elementAtomicNumber = clickedElement.getAttribute('data-linkedElement') || clickedElement.getAttribute('data-atomic');
+	const elementToShow = clickedElement.hasAttribute('data-linkedElement') ? document.querySelector(`[data-atomic="${elementAtomicNumber}"]`) : clickedElement;
+
+	showElementData(elementToShow);
 }
 function openLinkInIframe(rowId) {
 	const sitePopup = document.getElementById('sitePopup');
-	//var link = "Files/ElementsPDF/" + elementData[rowId].name + ".pdf#zoom=100&navpanes=0&page=1";
-	//var link = "https://mozilla.github.io/pdf.js/web/viewer.html?file=https://el.wikipedia.org/api/rest_v1/page/pdf/" + elementData[rowId].name + "#view=Fit"
-	//var link = "https://el.wikipedia.org/wiki/" + elementData[rowId].name;
+	//let link = "Files/ElementsPDF/" + elementData[rowId].name + ".pdf#zoom=100&navpanes=0&page=1";
+	//let link = "https://mozilla.github.io/pdf.js/web/viewer.html?file=https://el.wikipedia.org/api/rest_v1/page/pdf/" + elementData[rowId].name + "#view=Fit"
+	//let link = "https://el.wikipedia.org/wiki/" + elementData[rowId].name;
 	
-	var link;
+	let link;
+
+	function closePopup(event) {
+		sitePopup.querySelector('iframe').src = '';
+		sitePopup.style.display = "none";
+		sitePopup.querySelector('.close').removeEventListener('click', closePopup);
+		//document.querySelector('#sitePopup').removeEventListener('click', closePopup);
+	}
+
 	if (elementData[rowId].linkElementName) {
 		link = "https://mozilla.github.io/pdf.js/web/viewer.html?file=https://el.wikipedia.org/api/rest_v1/page/pdf/" + elementData[rowId].linkElementName + "#view=Fit";
 	} else {
@@ -230,12 +209,6 @@ function openLinkInIframe(rowId) {
 	sitePopup.querySelector('iframe').src = link;
 	
 	sitePopup.style.display = "block";
-	function closePopup(event) {
-		sitePopup.querySelector('iframe').src = '';
-		sitePopup.style.display = "none";
-		sitePopup.querySelector('.close').removeEventListener('click', closePopup);
-		//document.querySelector('#sitePopup').removeEventListener('click', closePopup);
-	}
 	sitePopup.querySelector('.close').addEventListener('click', closePopup);
 	//document.querySelector('#sitePopup').addEventListener('click', closePopup);
 }
@@ -244,10 +217,10 @@ function openLinkInIframe(rowId) {
 document.addEventListener('click', (event) => {
 	if (event.target.matches('.element')) {
 		elementClicked(event.target);
-		URL_setParam("SelectedElement", event.target.getAttribute('data-atomic') || event.target.getAttribute('data-linkedelement') );
+		URL_setParam("SelectedElement", event.target.getAttribute('data-atomic') || event.target.getAttribute('data-linkedElement') );
 	}
 });
 
 closeUp.addEventListener('click', function(event) {
-	infoElement()
+	infoElement();
 });
